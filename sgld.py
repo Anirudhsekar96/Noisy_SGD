14c6936aa12f1607ae734eb5691b66f1fcd2bf64
anirudhsekar96 <anirudhsekar96@gmail.com>
2019-05-13 20:40:58 -0400

    SGLD group gamma

--- sgld.py
+++ sgld.py
@@ -58,7 +58,7 @@ class SGLD(Optimizer):
 
                 #std = torch.pow(torch.div(group['noise'],torch.pow(state['step'].add(1),group['gamma'])),(0.5))
                 
-                std = torch.div(group['lr'],(state['step']**(state['gamma'])))
+                std = torch.div(group['lr'],(state['step']**(group['gamma'])))
                 nr = torch.normal(mean=0, std = std)
                 updt = grad.add(nr)
                 p.data.add_(-group['lr'],updt)
